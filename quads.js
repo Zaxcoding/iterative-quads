1eec077483e4959c341f88ce9e0e5489b79ef4b4
Zach Sadler <zaxcoding@gmail.com>
2015-02-22 18:09:12 -0500

    Lots of optimizations to make only one call to getPixelData and not array.sort

--- quads.js
+++ quads.js
@@ -4,9 +4,11 @@ var RectScores;
 
 var QuadsToStartOut = 4;
 var TargetImage = "owl.jpg";
-var DrawTimeout = 10;
+var DrawTimeout = 1;
 var AreaImportance = .85;
 var GridLines = false;
+
+var quadrantRGB = [4];
 
 // simple object to hold the score and corresponding rect
 function Score(score, rect) {
@@ -24,8 +26,6 @@ function Rect(startX, startY, width, height) {
 
 // find the rect with worst score, pop it out of the array, and iterate on it
 function draw(n) {
-	RectScores[n].sort(function(a, b) { return b.score - a.score; } );
-
 	iterate(RectScores[n].shift().rect, n);
 
 	setTimeout(function() { draw(n) }, DrawTimeout);
@@ -56,6 +56,55 @@ function reinitializeVariables() {
 		workingCanvas = document.getElementById('workingImg' + (i+1));
 	  workingImageCtxArray[i] = workingCanvas.getContext("2d");
 	}
+
+	var rawPixelData = targetImageCtx.getImageData(0, 0, 480, 640).data;
+	for (var i = 0; i < 4; i++) {
+		quadrantRGB[i] = [];
+		for (var j = 0; j < 240; j++) {
+			quadrantRGB[i][j] = [];
+			for (var k = 0; k < 320; k++) {
+				quadrantRGB[i][j][k] = new Object();
+			}
+		}
+	}
+
+	for (var i = 0; i < 480; i++) {
+		for (var j = 0; j < 640; j++) {
+			if (i < 240 && j < 320) {
+				quadrantRGB[0][i][j] = { r: rawPixelData[(j*480 + i)*4], g: rawPixelData[(j*480 + i)*4 + 1], b: rawPixelData[(j*480 + i)*4 + 2] };
+			}
+			if (i >= 240 && j < 320) {
+				quadrantRGB[1][i-240][j] = { r: rawPixelData[(j*480 + i)*4], g: rawPixelData[(j*480 + i)*4 + 1], b: rawPixelData[(j*480 + i)*4 + 2] };
+			}
+			if (i < 240 && j >= 320) {
+				quadrantRGB[2][i][j-320] = { r: rawPixelData[(j*480 + i)*4], g: rawPixelData[(j*480 + i)*4 + 1], b: rawPixelData[(j*480 + i)*4 + 2] };
+			}
+			if (i >= 240 && j >= 320) {
+				quadrantRGB[3][i-240][j-320] = { r: rawPixelData[(j*480 + i)*4], g: rawPixelData[(j*480 + i)*4 + 1], b: rawPixelData[(j*480 + i)*4 + 2] };
+			}
+		}
+	}
+}
+
+function locationOf(element, array, start, end) {
+	if (array.length == 0) return 0;
+
+  start = start || 0;
+  end = end || array.length;
+  var pivot = parseInt(start + (end - start) / 2, 10);
+  if (array[pivot] === element) return pivot;
+  if (end - start <= 1)
+    return array[pivot].score > element.score ? pivot : pivot - 1;
+  if (array[pivot].score < element.score) {
+		return locationOf(element, array, start, pivot);
+  } else {
+		return locationOf(element, array, pivot, end);
+  }
+}
+
+function insert(element, array) {
+  array.splice(locationOf(element, array) + 1, 0, element);
+  return array;
 }
 
 function setupAndGetGoing() {
@@ -88,6 +137,37 @@ function averageColorOfRect(rect, n) {
 	return ans;
 }
 
+function averageUsingArray(rect, n) {
+	var r = 0, g = 0, b = 0;
+
+	if (rect.width < 1 || rect.height < 1) {
+		return -Infinity;
+	}
+
+  var cnt = 0;
+	var flooredStartX = Math.floor(rect.startX);
+	var flooredStartY = Math.floor(rect.startY);
+
+	var endX = flooredStartX + rect.width;
+	var endY = flooredStartY + rect.height;
+
+	for (var i = flooredStartX; i < endX; i++) {
+		for (var j = flooredStartY; j < endY; j++) {
+			r += quadrantRGB[n][i][j].r;
+			g += quadrantRGB[n][i][j].g;
+			b += quadrantRGB[n][i][j].b;
+			cnt++;
+		}
+	}
+
+	r /= cnt;
+	g /= cnt;
+	b /= cnt;
+
+	ans = [r, g, b];
+	return ans;
+}
+
 // simple euclidean distance
 function differenceFromAverage(rgb1, rgb2) {
 	return (rgb1[0] - rgb2[0])*(rgb1[0] - rgb2[0]) + (rgb1[1] - rgb2[1])*(rgb1[1] - rgb2[1]) + (rgb1[2] - rgb2[2])*(rgb1[2] - rgb2[2]);
@@ -105,20 +185,20 @@ function iterate(rect, n) {
 		rect.height = 2;
 
 	// find the average color of the full rect and each quadrant
-	var wholeAverage = averageColorOfRect(rect, n);
+	var wholeAverage = averageUsingArray(rect, n);
 
 	var quads = [new Rect(rect.startX, rect.startY, rect.width/2, rect.height/2), new Rect(rect.startX + rect.width/2, rect.startY, rect.width/2, rect.height/2), new Rect(rect.startX, rect.startY + rect.height/2, rect.width/2, rect.height/2), new Rect(rect.startX + rect.width/2, rect.startY + rect.height/2, rect.width/2, rect.height/2)];
 
 	var quadAverages = [];
 	var differences = [];
 	for (var i = 0; i < 4; i++) {
-		quadAverages[i] = averageColorOfRect(quads[i], n);
+		quadAverages[i] = averageUsingArray(quads[i], n);
 		workingImageCtxArray[n].fillStyle = "#" + ("00" + Math.floor(quadAverages[i][0]).toString(16)).slice(-2)  + ("00" + Math.floor(quadAverages[i][1]).toString(16)).slice(-2) + ("00" + Math.floor(quadAverages[i][2]).toString(16)).slice(-2);
 		if (GridLines)
 			workingImageCtxArray[n].strokeRect(quads[i].startX, quads[i].startY, quads[i].width, quads[i].height);
 		workingImageCtxArray[n].fillRect(quads[i].startX, quads[i].startY, quads[i].width, quads[i].height);
 		differences[i] = differenceFromAverage(wholeAverage, quadAverages[i]);
-		RectScores[n].push(new Score(differences[i] * Math.pow(rect.width*rect.height, AreaImportance) , quads[i]));
+		RectScores[n] = insert(new Score(differences[i] * Math.pow(rect.width*rect.height, AreaImportance) , quads[i]), RectScores[n]);
 	}
 }
 
